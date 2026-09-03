6eff826418b3bd3466769d0d1c02ac053883ad45
Pavithra <pavimarriyappan@gmail.com>
2026-09-03 21:50:49 +0530

    Update Google Client ID to new vqdpama ID

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import './index.css'
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '637646427248-l03svtji4rck113fo92ehj8lbpafehv3.apps.googleusercontent.com';
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '637646427248-vqdpama7cu5e8tn0q91od64h8j2e46cu.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
